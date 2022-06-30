c1e0002c38ef3c519da077c9868352b7c0946189
Daniel Sarkesian <danielsarkesian@gmail.com>
2022-06-30 14:13:24 -0400

    worked on routes

--- app.js
+++ app.js
@@ -17,10 +17,36 @@ app.get("/items", function(req, res) {
 })
 
 app.post("/items", function(req, res) {
+    db.items.push({"name": req.body.name, "price": req.body.price})
+    console.log("db.items ==", db.items)
     return res.json({
-        name: req.params.name, price: req.params.price 
+        name: req.body.name, price: req.body.price
     })
 })
+
+app.get("/items/:name", function (req,res){
+    const name = req.params.name;
+    const item = db.items.find(i=> i.name===name)
+    console.log(db.items)
+
+
+    return res.json({item})
+})
+
+app.patch("/items/:name", function (req,res){
+    const name = req.params.name;
+    const item = db.items.find(i=> i.name===name)
+    let newname = req.body.name
+    let newPrice = req.body.price
+    item["name"]=newname
+    item["price"]=newPrice
+    console.log(item)
+
+
+    return res.json({"updated":{item}})
+
+})
+
 
 
 
@@ -30,7 +56,7 @@ app.post("/items", function(req, res) {
 app.use(function (req, res) {
     throw new NotFoundError();
   });
-  
+
   /** Error handler: logs stacktrace and returns JSON error message. */
   app.use(function (err, req, res, next) {
     const status = err.status || 500;
